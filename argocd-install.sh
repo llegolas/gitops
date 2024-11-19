60c077f690d08e9e3b39dcd8eb85fca01bfe1850
llegolas <kostadin.karaivanov@gmail.com>
2024-11-19 02:11:19 +0200

    example

---
+++ argocd-install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+DIRNAME=$(dirname $0)
+
+if [ -z ${ARGOCD_NS+x} ];then
+  ARGOCD_NS='argocd'
+fi
+
+if [ ! -z ${1+x} ]; then
+  if [ -d $1 ]; then
+    DIRNAME=$1  
+  else
+    echo "ERROR: Config directory $1 does not exist"
+    exit 1
+  fi
+fi
+
+VALUES_FILE="${DIRNAME}/values.yaml"
+
+if [ ! -f $VALUES_FILE ]; then
+  echo "ERROR: Config file $VALUES_FILE does not exists"
+  exit 1
+fi
+
+echo "INFO: Using values file $VALUES_FILE"
+
+echo "INFO: Argocd will be installed in $ARGOCD_NS namespace with values file $VALUES_FILE"
+echo -n "Do you want to proceed? [y/n]: "
+read ans
+if [ "$ans" == "y" ]; then
+  helm upgrade --install argocd argo/argo-cd \
+    --namespace=$ARGOCD_NS \
+    --create-namespace \
+    -f $VALUES_FILE
+
+  kubectl apply -n $ARGOCD_NS -f $DIRNAME/project.yaml
+  kubectl apply -n $ARGOCD_NS -f $DIRNAME/application.yaml
+else
+  echo "INFO: Exiting without action"
+  exit 0
+fi
