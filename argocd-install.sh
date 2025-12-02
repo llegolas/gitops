c8482c903a3f561f67b4bc4c9452cd75a2d59772
llegolas <kostadin.karaivanov@gmail.com>
2025-12-03 01:29:00 +0200

    improvements

--- argocd-install.sh
+++ argocd-install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+set -euo pipefail
+
 DIRNAME=$(dirname $0)
-HELM_VERSION=8.3.3
+HELM_VERSION=5.51.0
+CRD_VERSION=v2.10.5
 
 if [ -z ${ARGOCD_NS+x} ];then
   ARGOCD_NS='argocd'
@@ -25,16 +29,14 @@ fi
 echo "INFO: Using values file $VALUES_FILE"
 
 echo "INFO: Argocd will be installed in $ARGOCD_NS namespace with values file $VALUES_FILE"
-echo -n "Do you want to proceed? [y/n]: "
-read ans
-if [ "$ans" == "y" ]; then
-  kubectl kustomize https://github.com/argoproj/argo-cd.git/manifests/crds/ | kubectl apply -f -
+if [ "${1-}" == "-y" ] || [ "${1-}" == "--yes" ]; then
+  kubectl apply -k "https://github.com/argoproj/argo-cd/manifests/crds?ref=${CRD_VERSION}"
   helm upgrade --install argocd argo/argo-cd \
     --namespace=$ARGOCD_NS \
     --version $HELM_VERSION \
     --create-namespace \
     -f $VALUES_FILE
 else
-  echo "INFO: Exiting without action"
+  echo "INFO: Dry-run. Exiting without action. Use -y or --yes to install."
   exit 0
 fi
